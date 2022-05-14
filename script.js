2a5ebe190cff27100a24f7cc5a54c04e0e40a215
zxcwer <61655281+zxcwer@users.noreply.github.com>
2022-05-14 17:00:41 +0900

    main

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const body = document.querySelector('body');
 
 toggle.addEventListener('click', function() {
     this.classList.toggle('bi-moon');
-    if (this.classList.toggle('bi-brightness-high-fill')) {
+    if (this.classList.toggle('bi-brightness-high')) {
         body.style.background = 'white';
         body.style.color = 'black';
         body.style.transition = '2s';
